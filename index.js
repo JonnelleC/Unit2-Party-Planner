1b5c084d0c2a16bb2eb20c9ddf61fe5a781b3f92
JonnelleC <carrjonnelle@gmail.com>
2024-07-13 00:08:08 -0400

    4th commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const state = {
-  parties: [],
-  party: {},
-};
+// const state = {
+//   parties: [],
+//   party: {},
+// };
 
 document.addEventListener('DOMContentLoaded', async ()=> {
 
@@ -28,6 +28,29 @@ const getParties = async () => {
   getParties();
 };
 
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async(event) => {
+    event.preventDefault();
+    try{
+        const partyName = document.getElementById('eventName').value;
+        const partyDescription = document.getElementById('eventDescription').value;
+        const partyTime = document.getElementById('eventTime').value;
+
+        const newParty = document.createElement('li');
+        newParty.innerHTML = 
+        `
+        <li>Name:${name}</li>
+        <li>Description:${description}</li>
+         <li>Time: ${time} </li>
+          <li>date:${date}</li>
+        `;
+        document.getElementById('events').appendChild(newParty);
+    }catch (error) {
+        console.error("Could not fetch resource", error);
+      }
+
+})
 
 });
 
