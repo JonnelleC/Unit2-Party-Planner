a796f377a7127a0ab1f83548bb5e586a43572512
JonnelleC <carrjonnelle@gmail.com>
2024-07-12 23:27:37 -0400

    3rd commit

--- index.js
+++ index.js
@@ -3,32 +3,31 @@ const state = {
   party: {},
 };
 
+document.addEventListener('DOMContentLoaded', async ()=> {
+
+
 const getParties = async () => {
   try {
     const response = await fetch(
-      `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events`
-    );
+      `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events` );
     const partyData = await response.json();
+    const eventContainer = document.querySelector("ol");
+    eventsContainer.innerHTML = '';
+    partyData.forEach(event => {
+        const eventElement = document.createElement('li');
+        eventElement.innerHTML =`
+        <li>Date: ${event.date} </li>
+        <li>Time: ${event.Time} </li>
+        <li>Location: ${event.location}</li>
+        <li>Description:${event.description}`;
+        eventsContainer.appendChild(eventElement);
+    })
   } catch (error) {
-    throw new error("Could not fetch resource");
+    console.error("Could not fetch resource", error);
   }
+  getParties();
 };
 
-getParties();
-const getParty = async (eventName) => {
-  try {
-    const response = await fetch(
-      `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events${eventName}`
-    );
-    const partyName = await response.json;
-    const event = partyName;
-  } catch (error) {
-    throw new error("Could not fetch resource");
-  }
-};
 
-const createEventListing = () => {
+});
 
-}
-
-const partyListing = partydata.names
