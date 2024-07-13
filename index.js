9bf66f820c42508761e78ef5f254cb73891d6a12
JonnelleC <carrjonnelle@gmail.com>
2024-07-12 22:57:06 -0400

    completed html for that allows user to create a new event listing

--- index.js
+++ index.js
@@ -15,13 +15,20 @@ const getParties = async () => {
 };
 
 getParties();
-const getParty = async (partyName) => {
+const getParty = async (eventName) => {
   try {
     const response = await fetch(
       `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events${eventName}`
     );
     const partyName = await response.json;
+    const event = partyName;
   } catch (error) {
     throw new error("Could not fetch resource");
   }
 };
+
+const createEventListing = () => {
+
+}
+
+const partyListing = partydata.names
